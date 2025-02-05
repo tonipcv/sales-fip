9970a652272424be1467b09b3a0fbe4357f5a4fa
tonipcv <xppsalvador@gmail.com>
2025-02-05 13:59:20 +0000

    dsa

---
+++ src/app/suporte/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from 'next/navigation';
+
+export default function RedirectPage() {
+  redirect('https://api.whatsapp.com/send?phone=557391778075&text=Ol%C3%A1%2C%20quero%20tirar%20d%C3%BAvidas%20do%20Cronograma!');
+} 
